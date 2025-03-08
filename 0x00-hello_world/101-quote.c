eff3abec4eaeb9bdcb5dfc968a0c4f2f2da1fa0f
berehan-fel <berehan.fel@gmail.com>
2025-03-08 20:47:46 +0300

    Update 101-quote.c, make betty linter compliant

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,6 @@
 
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar,"
-			"2015-10-19\n";
-
-	write(2, msg, sizeof(msg) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
