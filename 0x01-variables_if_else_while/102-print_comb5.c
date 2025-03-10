a2a5197a68650d5e6a34640b30543d2cd0508165
berehan-fel <berehan.fel@gmail.com>
2025-03-10 16:17:57 +0300

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = i; k <= '9'; k++)
+			{
+				for (l = j + 1; l <= '9'; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i != '9' || j != '8')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
