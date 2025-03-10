4e5f4055d0751c25153fb169b32597b638b44fe5
berehan-fel <berehan.fel@gmail.com>
2025-03-10 17:00:27 +0300

    Create 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,11 @@ int main(void)
 		{
 			for (k = i; k <= '9'; k++)
 			{
-				for (l = j + 1; l <= '9'; l++)
+				if (k > i)
+					l = '0';
+				else
+					l = j + 1;
+				for (; l <= '9'; l++)
 				{
 					putchar(i);
 					putchar(j);
