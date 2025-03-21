6881131b8d1bcd6945537b8d612587726644c9d4
berehan-fel <berehan.fel@gmail.com>
2025-03-21 16:29:51 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,16 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * main - prints the number of arguments passed into it.
- * @ac: the number of command line arguments passed to the program.
- * @av: a vector of the command line arguments.
+ * @argc: the number of command line arguments passed to the program.
+ * @argv: a vector of the command line arguments.
  *
  * Return: Always 0 (success).
  */
 
-int main(int ac, char **av __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", ac - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
