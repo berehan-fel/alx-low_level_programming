252a20e72c4f9770387619b0b5e2a85c547f674e
berehan-fel <berehan.fel@gmail.com>
2025-03-25 14:03:40 +0300

    Update 3-alloc_grid.c, free every allocated memory if malloc failed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,12 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
