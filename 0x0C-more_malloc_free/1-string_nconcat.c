7e6ada6a7f3ba4e1f0b9a5c993f5d451d8831e5f
berehan-fel <berehan.fel@gmail.com>
2025-03-26 15:10:16 +0300

    second update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -39,18 +39,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 
+	if (len2 >= n)
+		len2 = n;
+
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (s == NULL)
 		return (NULL);
-	if (len2 >= n)
-		len2 = n;
 
 	for (i = 0; i < len1; i++)
 		s[i] = s1[i];
 	for (j = 0; j < len2; j++)
 		s[i++] = s2[j];
+
 	s[i] = '\0';
 	return (s);
 }
-
