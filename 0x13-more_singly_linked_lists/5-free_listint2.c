9d070ecd8e39a3864f6ed0dd1a8d516dbb14b0f8
berehan-fel <berehan.fel@gmail.com>
2025-04-08 14:29:36 +0300

    Create 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - frees a listint_t list and sets head to NULL
+ * @head: double pointer to the first node of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+	head = NULL;
+}
