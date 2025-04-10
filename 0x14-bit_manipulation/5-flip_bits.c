eb0f3cbe536dfb792f8d464923c505a49996010e
berehan-fel <berehan.fel@gmail.com>
2025-04-10 20:52:59 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,13 +9,15 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int bits = 0;
-	unsigned int i;
+	unsigned int xor = n ^ m;
+	unsigned int count = 0;
 
-	for (i = 0; i < sizeof(n) * 8 - 1; i++)
-		bits += ((m >> i) & 1UL) ^ ((n >> i) & 1UL);
-
-	return (bits);
+	while (xor)
+	{
+		count += (xor & 1);
+		xor >>= 1;
+	}
+	return (count);
 }
 
 
